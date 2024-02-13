08d3ed7eba297a0d099b6fc5a1944299c9637554
VanGuardf <alisaadati18000@gmail.com>
2024-02-13 23:07:56 +0330

    Target to page element in script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container");
+const search = document.querySelector(".search-box button");
+const weatherBox = document.querySelector(".weather-box");
+const weathreDetails = document.querySelector(".weather-detail");
+const error404 = document.querySelector(".not-found");
+const cityHide = document.querySelector(".city-hide");
